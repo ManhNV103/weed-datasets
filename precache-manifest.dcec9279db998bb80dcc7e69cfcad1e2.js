32d4d1eb8f5d379da8172643f18a121f03398528
Mike Nguyen <manhdn98@gmail.com>
2020-01-21 19:57:55 +1000

    Updates

---
+++ precache-manifest.dcec9279db998bb80dcc7e69cfcad1e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548648bb4b24616699be6d870ceb755f",
+    "url": "/weed-datasets/index.html"
+  },
+  {
+    "revision": "a489809d71b68e67e55f",
+    "url": "/weed-datasets/static/css/main.7e81c441.chunk.css"
+  },
+  {
+    "revision": "d7aaeaf602fda77efda3",
+    "url": "/weed-datasets/static/js/2.0d950845.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/weed-datasets/static/js/2.0d950845.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a489809d71b68e67e55f",
+    "url": "/weed-datasets/static/js/main.226f9f9f.chunk.js"
+  },
+  {
+    "revision": "00999ded9f1018c2e6ee",
+    "url": "/weed-datasets/static/js/runtime-main.0d7c3f3f.js"
+  }
+]);
